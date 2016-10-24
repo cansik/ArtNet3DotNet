086e28f79b8a94a901c5cf873be8e5968edc7bf3
Florian Bruggisser <florian@nexpose.ch>
2016-10-24 17:42:00 +0200

    implemented artnet viewer in terminal

--- ArtDotNet/ArtDotNetClient/Program.cs
+++ ArtDotNet/ArtDotNetClient/Program.cs
@@ -6,20 +6,48 @@ namespace ArtDotNetClient
 {
 	class MainClass
 	{
+
 		public static void Main(string[] args)
 		{
+			var subUni = 0;
+			var running = true;
+
 			Console.WriteLine("ArtDotNet Client");
 			var controller = new ArtNetController();
 			controller.Address = IPAddress.Loopback;
 
 			controller.DmxPacketReceived += (s, p) =>
 			{
-				Console.WriteLine("DMX Packet received " + p.Length);
+				if (p.SubUni != subUni)
+					return;
+
+				Console.Clear();
+				Console.WriteLine("ArtNet Universe " + subUni);
+
+				for (int i = 0; i < p.Length; i++)
+				{
+					if (i % 24 == 0)
+						Console.WriteLine();
+
+					Console.Write(string.Format("{000:00} ", p.Data[i]));
+				}
 			};
 
 			controller.Start();
 
-			Console.ReadKey(true);
+			while (running)
+			{
+				var key = Console.ReadKey(true);
+
+				if (key.Key == ConsoleKey.UpArrow)
+					subUni++;
+
+				if (key.Key == ConsoleKey.DownArrow)
+					subUni--;
+
+				if (key.Key == ConsoleKey.Escape)
+					running = false;
+			}
 
 			controller.Stop();
 		}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ArtNet3DotNet
 ArtNet 3 Implementation for .NET in C#
+
+## Example
+
+```
+Console.WriteLine("ArtDotNet Client");
+var controller = new ArtNetController();
+controller.Address = IPAddress.Loopback;
+
+controller.DmxPacketReceived += (s, p) =>
+{
+	Console.WriteLine("DMX Packet received " + p.Length);
+};
+
+controller.Start();
+
+Console.ReadKey(true);
+
+controller.Stop();
+```
